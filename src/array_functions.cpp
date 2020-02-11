6e408eb1a38439666433c60b58e12bba955e124d
th3yubbs <theyubbs@gmail.com>
2020-02-10 21:57:45 -0500

    Worked on stuff

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -1,79 +1,96 @@
 /*
  * functionstocomplete.cpp
  *
- *  Created on: Sep 10, 2017
- *      Author: keith
+ *  Created on: Feb. 10th 2020
+ *      Author: Alexander Stubbs-Yates
  */
 
 //============================================================================
 //	TODO add necessary includes here
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "array_functions.h"
 #include "constants.h"
+#include "utilities.h"
 //============================================================================
 using namespace std;
+using namespace constants;
 //============================================================================
 //	stuff you will need
 //============================================================================
 //TODO define a structure to track words and number of times they occur
-struct Stuff{
+struct tracking{
 	int count;
 	string word;
 };
 
 //TODO add a global array of entry structs (global to this file)
+struct tracking wordArray[MAX_WORDS];
 
 //TODO add variable to keep track of next available slot in array
 int nextSlot;
+
 //TODO define all functions in header file
+//******GOTTA DO
 
 //zero out array that tracks words and their occurrences
 void clearArray() {
-
+	nextSlot=0;
 }
 
 //how many unique words are in array
 int getArraySize() {
-	return -1;
+	return nextSlot;
 }
 
 //get data at a particular location
-std::string getArrayWordAt(int i) {
-	return "";
+string getArrayWordAt(int i) {
+	return wordArray[i].word;
 }
 
 int getArrayWord_NumbOccur_At(int i) {
-	return -1;
+	return wordArray[i].count;
 }
 
 /*loop through whole file, one line at a time
  * call processLine on each line
  * returns false: myfstream is not open
  *         true: otherwise*/
-bool processFile(std::fstream &myfstream) {
-	return false;
+bool processFile(fstream &myfstream) {
+	if(myfstream.is_open()){
+
+	}
+	else{
+		return false;
+	}
+
 }
 
 /*take 1 line and extract all the tokens from it
 feed each token to processToken for recording*/
-void processLine(std::string &myString) {
+void processLine(string &myString) {
 
 }
 
 /*Keep track of how many times each token seen*/
-void processToken(std::string &token) {
+void processToken(string &token) {
 
 }
 
 /*if you are debugging the file must be in the project parent directory
   in this case Project2 with the .project and .cProject files*/
-bool openFile(std::fstream& myfile, const std::string& myFileName,
-		std::ios_base::openmode mode) {
+bool openFile(fstream& myfile, const string& myFileName,
+		ios_base::openmode mode) {
 	return false;
 }
 
 /*iff myfile is open then close it*/
-void closeFile(std::fstream& myfile) {
-
+void closeFile(fstream& myfile) {
+	if (myfile.is_open()){
+		myfile.close();
+	}
 }
 
 /* serializes all content in myEntryArray to file outputfilename
@@ -81,7 +98,7 @@ void closeFile(std::fstream& myfile) {
  * 			FAIL_NO_ARRAY_DATA if there are 0 entries in myEntryArray
  * 			SUCCESS if all data is written and outputfilename closes OK
  * */
-int writeArraytoFile(const std::string &outputfilename) {
+int writeArraytoFile(const string &outputfilename) {
 	return -1;
 }
 
@@ -89,7 +106,7 @@ int writeArraytoFile(const std::string &outputfilename) {
  * Sort myEntryArray based on so enum value.
  * The presence of the enum implies a switch statement
  */
-void sortArray(constants::sortOrder so) {
+void sortArray(sortOrder so) {
 
 }
 
